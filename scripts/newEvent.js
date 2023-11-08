e4fb1c6faac623703963549ec0ab3f0869d5ddb4
Sohail Grewal <sgrewal118@my.bcit.ca>
2023-11-08 11:54:56 -0800

    fixed typo in newEvent

--- scripts/newEvent.js
+++ scripts/newEvent.js
@@ -1,18 +1,18 @@
 
 function postEvent() {
-    var newEvent = db.collection("events").doc();
-    var eventName = document.getElementById("eventCreationFrom").value;
-    var description = document.getElementById("eventDescription").value;
+    // var newEvent = db.collection("events").doc();
+    var eventName = document.getElementById("eventName").value;
+    var description = document.getElementById("description").value;
     var date = document.getElementById("eventDate").value;
     var location = document.getElementById("eventLocation").value;
 
+    console.log(eventName, description);
+
     db.collection("events").add({
         name: eventName,
-        description: description,
-        date: date,
-        location: location
-    })
-    console.log(eventName, description);
+        description: description
+    });
+    
 
 }
 
