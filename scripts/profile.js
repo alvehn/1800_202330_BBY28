5b4314a4d64bf7003337a1ecbeb7dcd83ca5fb58
Alvin Hu <alvehnhu@gmail.com>
2023-11-08 13:42:05 -0800

    alvin stuff

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,26 +1,27 @@
 
 
-function displayUser() {
-    let params = new URL(window.location.href); //get URL of search bar
-    console.log(params);
-    let ID = params.searchParams.get("docID"); //get value for key "id"
-    console.log(ID);
+// function displayUser() {
+//     let params = new URL(window.location.href); //get URL of search bar
+//     console.log(params);
+//     let ID = params.searchParams.get("docID"); //get value for key "id"
+//     console.log(ID);
 
-    db.collection("users")
-        .doc(ID)
-        .get()
-        .then(doc => {
-            var title = doc.data().name;                // get value of the "name" 
+//     db.collection("users")
+//         .doc(ID)
+//         .get()
+//         .then(doc => {
+//             var userName = doc.data().name;                // get value of the "name" 
 
-            // populates name, location, title, and description
-            document.getElementById("name-goes-here").innerHTML = title;
+//             // populates name
+//             document.getElementById("name-goes-here").innerText = userName;  
 
-            // let imgEvent = document.querySelector( ".hike-img" );
-            // imgEvent.src = "../images/" + hikeCode + ".jpg";
-        });
-}
-displayUser();
+//             // let imgEvent = document.querySelector( ".hike-img" );
+//             // imgEvent.src = "../images/" + hikeCode + ".jpg";
+//         });
+// }
+// displayUser();
 
+// function getNameFromAuth() {
 //     firebase.auth().onAuthStateChanged(user => {
 //         // Check if a user is signed in:
 //         if (user) {
@@ -43,27 +44,38 @@ displayUser();
 //         }
 //     });
 // }
+//getNameFromAuth();
 
-// getNameFromAuth(); //run the function
+var currentUser;               //points to the document of the user who is logged in
+function populateUserInfo() {
+            firebase.auth().onAuthStateChanged(user => {
+                // Check if user is signed in:
+                if (user) {
 
-// function editUserInfo() {
-//     //Enable the form fields
-//     document.getElementById('personalInfoFields').disabled = false;
-//  }
+                    //go to the correct user document by referencing to the user uid
+                    currentUser = db.collection("users").doc(user.uid)
+                    //get the document for current user.
+                    currentUser.get()
+                        .then(userDoc => {
+                            //get the data fields of the user
+                            // var userName = userDoc.data().name;
+                            console.log(user.displayName);  //print the user name in the browser console
+                            userName = user.displayName;
 
-//  function saveUserInfo() {
-//     //enter code here
+                            document.getElementById("name-goes-here").innerText = userName;
+                            
 
-//     userName = document.getElementById('nameInput').value;       //get the value of the field with id="nameInput"
+                            //if the data fields are not empty, then write them in to the form.
+                            if (userName != null) {
+                                document.getElementById("name-goes-here").value = userName;
+                            }
+                        })
+                } else {
+                    // No user is signed in.
+                    console.log ("No user is signed in");
+                }
+            });
+        }
 
-
-//     currentUser.update({
-//         name: userName,
-        
-//     })
-//     .then(() => {
-//         console.log("Document successfully updated!");
-//     })
-
-//     document.getElementById('personalInfoFields').disabled = true;
-// }
+//call the function to run it 
+populateUserInfo();
