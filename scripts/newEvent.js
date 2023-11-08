85dae08dcd28a6e5f73a4355ffa6c4cde67d9b4d
Sohail Grewal <sgrewal118@my.bcit.ca>
2023-11-08 11:10:37 -0800

    small changes to newEventt

--- scripts/newEvent.js
+++ scripts/newEvent.js
@@ -2,16 +2,16 @@
 function postEvent() {
     var newEvent = db.collection("events").doc();
     var eventName = document.getElementById("eventCreationFrom").value;
-    var description = document.getElementById("description").value;
+    var description = document.getElementById("eventDescription").value;
     var date = document.getElementById("eventDate").value;
     var location = document.getElementById("eventLocation").value;
 
-    console.log(eventName, description);
-
     db.collection("events").add({
         name: eventName,
-        description: description
-    });
+        description: description,
+        date: date,
+        location: location
+    })
     console.log(eventName, description);
 
 }
