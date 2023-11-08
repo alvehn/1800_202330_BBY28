0f613c11f19c47b1c9afc3efe4c5242fc24293b1
Alvin Hu <alvehnhu@gmail.com>
2023-11-07 19:16:28 -0800

    alvin stuff

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,31 +1,28 @@
-var currentUser;               //points to the document of the user who is logged in
-function populateUserInfo() {
-            firebase.auth().onAuthStateChanged(user => {
-                // Check if user is signed in:
-                if (user) {
+function getNameFromAuth() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if a user is signed in:
+        if (user) {
+            // Do something for the currently logged-in user here: 
+            console.log(user.uid); //print the uid in the browser console
+            console.log(user.displayName);  //print the user name in the browser console
+            userName = user.displayName;
 
-                    //go to the correct user document by referencing to the user uid
-                    currentUser = db.collection("users").doc(user.uid)
-                    //get the document for current user.
-                    currentUser.get()
-                        .then(userDoc => {
-                            //get the data fields of the user
-                            var userName = userDoc.data().name;
+            //method #1:  insert with JS
+            document.getElementById("name-goes-here").innerText = userName;    
 
-                            //if the data fields are not empty, then write them in to the form.
-                            if (userName != null) {
-                                document.getElementById("nameInput").value = userName;
-                            }
-                        })
-                } else {
-                    // No user is signed in.
-                    console.log ("No user is signed in");
-                }
-            });
+            //method #2:  insert using jquery
+            //$("#name-goes-here").text(userName); //using jquery
+
+            //method #3:  insert using querySelector
+            //document.querySelector("#name-goes-here").innerText = userName
+
+        } else {
+            // No user is signed in.
         }
+    });
+}
 
-//call the function to run it 
-populateUserInfo();
+getNameFromAuth(); //run the function
 
 function editUserInfo() {
     //Enable the form fields
