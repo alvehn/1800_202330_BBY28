b1ed822fab48b6c27e4c5a61e5d6a919ee04ad2b
Sohail Grewal <sgrewal118@my.bcit.ca>
2023-11-08 15:34:53 -0800

    made other forms in event creation write to database

--- scripts/newEvent.js
+++ scripts/newEvent.js
@@ -1,18 +1,29 @@
 
 function postEvent() {
-    // var newEvent = db.collection("events").doc();
-    var eventName = document.getElementById("eventName").value;
-    var description = document.getElementById("description").value;
-    var date = document.getElementById("eventDate").value;
-    var location = document.getElementById("eventLocation").value;
+    var user = firebase.auth().currentUser;
+    if (user) {
+        var currentUser = db.collection("users").doc(user.uid);
+        var userID = user.uid;
+        // var newEvent = db.collection("events").doc();
+        var eventName = document.getElementById("eventName").value;
+        var description = document.getElementById("description").value;
+        var date = document.getElementById("eventYear").value + "-" 
+            + document.getElementById("eventMonth").value + "-"
+            + document.getElementById("eventDay").value;
+        
+        var location = document.getElementById("locationOptions").value;
 
-    console.log(eventName, description);
+        console.log(eventName, description, date);
 
-    db.collection("events").add({
-        name: eventName,
-        description: description
-    });
-    
+        db.collection("events").add({
+            host: userID,
+            name: eventName,
+            description: description,
+            date: new Date(date)
+        });
+    } else {
+        console.log("No user is signed in");
+    }
 
 }
 
