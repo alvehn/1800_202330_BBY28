2e9b0c18f4e1a5bab06f2e43bcf6148de8343323
Alvin Hu <alvehnhu@gmail.com>
2023-11-12 14:00:52 -0800

    you can now change user name (edit profile info)

--- scripts/profile.js
+++ scripts/profile.js
@@ -62,13 +62,15 @@ function populateUserInfo() {
                             console.log(user.displayName);  //print the user name in the browser console
                             userName = user.displayName;
 
-                            document.getElementById("name-goes-here").innerText = userName;
+                            if (userName != null) {
+                                document.getElementById("nameInput").value = userName;
+                            }
+
+                            // document.getElementById("name-goes-here").innerText = userName;
                             
 
                             //if the data fields are not empty, then write them in to the form.
-                            if (userName != null) {
-                                document.getElementById("name-goes-here").value = userName;
-                            }
+                            
                         })
                 } else {
                     // No user is signed in.
@@ -79,3 +81,24 @@ function populateUserInfo() {
 
 //call the function to run it 
 populateUserInfo();
+
+function editUserInfo() {
+    //Enable the form fields
+    document.getElementById('personalInfoFields').disabled = false;
+ }
+
+ function saveUserInfo() {
+    //enter code here
+
+    userName = document.getElementById('nameInput').value;       //get the value of the field with id="nameInput"
+
+
+    currentUser.update({
+        name: userName,
+    })
+    .then(() => {
+        console.log("Document successfully updated!");
+    })
+
+    document.getElementById('personalInfoFields').disabled = true;
+}
