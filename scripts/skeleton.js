92b6183b947fceb07f6dab054e06ad5d40fe484c
Navjot Kehal <navjotkehal5@gmail.com>
2023-11-30 19:00:10 -0800

    using skeleton.js to load footer on every page

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,14 +1,11 @@
 function loadSkeleton() {
 
     firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {                   //if the pointer to "user" object is not null, then someone is logged in
-            // User is signed in.
-            // Do something for the user here.
-            console.log($('#navbarPlaceholder').load('nav_after_login.html'));
-        } else {
-            // No user is signed in.
-           // console.log($('#navbarPlaceholder').load('nav_before_login.html'));
-        }
+        //if the pointer to "user" object is not null, then someone is logged in
+        if (user) {                   
+            // shows navbar only signed in users can see
+            $('#navbarPlaceholder').load('nav_after_login.html');
+        } 
     });
 }
 loadSkeleton(); //invoke the function
